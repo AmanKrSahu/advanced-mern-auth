285c386eed10c256c3fe64ab5660d06b839035f2
Aman <amankrsahu003@gmail.com>
2025-06-08 22:11:15 +0530

    middleware(error): added error handling logic in middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,39 @@
 import "dotenv/config";
 
 import cors from "cors";
-import express, {Request, Response} from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import { config } from "./config/app.config";
+import connectDatabase from "./database/database";
+import { errorHandler } from "./middlewares/errorHandler";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: config.APP_ORIGIN,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: config.APP_ORIGIN,
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 
-app.get("/", (req:Request, res:Response) => {
-  res.status(200).json({
-    message: "Server started successfully"
-  });
-});
+app.get(
+  "/",
+  asyncHandler(async (_req: Request, res: Response, _next: NextFunction) => {
+    res.status(HTTPSTATUS.OK).json({
+      message: "Server started successfully",
+    });
+  })
+);
+
+app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
   console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}.`);
+  await connectDatabase();
 });
